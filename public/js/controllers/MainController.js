1c3c5e41d6bc929b3d45aa812264dae160bdf7da
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-05-12 18:09:01 -0400

    added ng-repeat

---
+++ public/js/controllers/MainController.js
@@ -0,0 +1,11 @@
+app.controller('MainController', function ($scope, Homework) {
+    //$scope.flashCards = whateverName;
+    //console.log(FlashCardsFactory.getFlashCards());
+
+    $scope.assignments = Homework;
+
+
+
+
+
+});
---
+++ public/js/factories/HomeworkFactory.js
@@ -0,0 +1,33 @@
+app.factory('Homework', function () {
+
+    return [{
+        assignment: 'Test First JavaScript',
+        url: 'http://github.com/myusername/TestFirst',
+        status: 'completed'
+    },
+        {
+            assignment: 'Angular Day 1',
+            url: 'http://github.com/myusername/TestFirst',
+            status: 'completed'
+        },
+
+        {
+            assignment: 'Angular Day 2',
+            url: 'http://github.com/myusername/TestFirst',
+            status: 'completed'
+        },
+
+        {
+            assignment: 'Angular Day 3',
+            url: 'http://github.com/myusername/TestFirst',
+            status: 'Started'
+        },
+
+        {
+            assignment: 'Angular Day 4',
+            url: 'http://github.com/myusername/TestFirst',
+            status: 'Not Started'
+        }];
+
+
+});
